e0b14a66b6ad5649ea0707d6f9ffed1134426ff8
Edugolr <christofer.wikman@gmail.com>
2023-03-23 23:30:44 +0100

    Add css custom prop

--- src/App.vue
+++ src/App.vue
@@ -18,33 +18,39 @@ body {
   margin: 0;
   height: 100vh;
   background-color: #FFF7DB;
-  font-family: 'Glacial Indifference', sans-serif;
+  font-family: var(--font-primary);
   font-size: 16px;
+  background-image: url(/src/assets/logo.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-blend-mode: overlay;
 }
 
 p {
-  font-family: 'Glacial Indifference', sans-serif;
+  font-family: var(--font-primary);
   font-size: 1rem;
   margin-block: 0;
   text-align: center;
 }
 
 h1 {
-  font-family: 'Cormorant Garamond', serif;
+  font-family: var(--font-secondary);
   font-size: 3rem;
   text-align: center;
 
 }
 
 h2 {
-  font-family: 'Cormorant Garamond', serif;
+  font-family: var(--font-secondary);
   font-size: 1.5rem;
   margin-block-end: 0.5em;
   text-align: center;
+  color: var(--color-h2);
 }
 
 h3 {
-  font-family: 'Cormorant Garamond', serif;
+  font-family: var(--font-secondary);
   font-size: 1.25rem;
   margin-block-end: 0.5em;
   text-align: center;
