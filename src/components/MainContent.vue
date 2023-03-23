d44b86a0acd1672e1086480900e4f5a6d2aeb476
Edugolr <christofer.wikman@gmail.com>
2023-03-23 23:30:51 +0100

    Add logo

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -3,6 +3,9 @@
         <slot />
     </div>
 </template>
+<script setup lang="ts">
+const logo = new URL('@/assets/logo.svg', import.meta.url).href
+</script>
 <style scoped>
 .main-content {
     padding: 20px;
